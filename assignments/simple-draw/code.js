34feaadb95c167ccc0d59f0b1b965f5fa1060c0d
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-18 15:17:37 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -37,6 +37,6 @@
 
 drawFilledRect(0, height - 1000, 1000, 1000, 'blue')
 
-drawLine(0, height, 82.5, 500, 'black', 20);
+drawLine(0, height, 82.5, 700, 'black', 20);
 drawLine(0, height, width, 0, 'black', 20);
 drawLine(0, height, width, height, 'black', 20);
