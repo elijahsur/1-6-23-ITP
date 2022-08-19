7732aa404cb065be9a0319514ed046051567afd3
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-19 15:26:18 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -45,5 +45,5 @@ drawLine(width,0,0,0,'black',30);
 drawLine(0,0,0,height,'black',30);
 drawLine(0,height,width,height,'black',30);
 drawLine(width/2-160,height/2+80,width/2+160,height/2+80,'black',20)
-drawLine(width/2-120,height/2+120,width/2+120,height/2+120,'black',20)
-drawLine(width/2-80,height/2+160,width/2+80,height/2+160,'black',20)
+drawLine(width/2-120,height/2+120,width/2+120,height/2+120,'orange',20)
+drawLine(width/2-80,height/2+160,width/2+80,height/2+160,'blue',20)
