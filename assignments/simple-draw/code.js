8d78d08845c7498131e568bc05fc2eff064f5be4
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-19 15:08:28 -0700

    Updating assignments/simple-draw/code.js

--- assignments/simple-draw/code.js
+++ assignments/simple-draw/code.js
@@ -42,4 +42,5 @@ drawLine(0, height, width, height, 'black', 20);
 drawLine(0, height, 0, 0, 'black', 20);
 drawLine(0, 0, width, 0, 'black', 20);
 drawLine(width, 0, width, height, 'black', 20);
-drawCircle(width/2, height/2, 40, 'black', linewidth=5);
+drawFilledRect(width/2-25, height/2-25, 50, 50, 'black');
+draw
