7287e7fd2cda9c4361f9e20aec176f65a38eeee4
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-25 15:26:27 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -1,23 +1,6 @@
-// This is an example of a function definition. This function is called by the
-// underlying animation framework thanks to the call to animate below. This
-// function is responsible for drawing one frame of the animation. You can
-// change the code in here using the same functions you had in the simple
-// drawing exercise to draw shapes. The argument to this function, time, is the
-// number of milliseconds (one millionth of a second) since the program started.
 const drawFrame = (time) => {
   clear();
   drawFilledCircle((time / 10) % width, height, 50, 'purple');
+  drawFilledCircle((time / 10) % width, -height, 50, 'purple');
 };
-
-// This is a function that we define to make it easier to draw a triangle. You
-// may want to experiment with writing your own functions using this one as a
-// model to draw other shapes.
-// This is a function that we define to make it easier to draw a triangle. You
-// may want to experiment with writing your own functions using this one as a
-// model to draw other shapes.
-
-
-// Leave this code here or the animation won't run. Also don't change the name
-// of drawFrame either here or where it is defined. (Or, if you must, change it
-// the same way in both places.)
 animate(drawFrame);
