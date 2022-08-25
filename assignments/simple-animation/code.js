eb149ea930986693d24fb64c2f4a8aeb1f2af836
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-25 15:25:14 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -21,27 +21,7 @@ const drawFrame2 = (time) => {
 // This is a function that we define to make it easier to draw a triangle. You
 // may want to experiment with writing your own functions using this one as a
 // model to draw other shapes.
-const drawTriangle = (x1, y1, x2, y2, x3, y3, color, width = 1) => {
-  drawLine(x1, y1, x2, y2, color, width);
-  drawLine(x2, y2, x3, y3, color, width);
-  drawLine(x3, y3, x1, y1, color, width);
-};
-// This draws a falling trangle of a particular shape whose bottom point is
-// positioned at x and whose y is a function of time.
-const drawFallingTriangle = (x, time) => {
-  // Figure out the x values relative to the passed in x
-  let x1 = x - 100;
-  let x2 = x;
-  let x3 = x + 100;
 
-  // Figure out the y values as a function of time.
-  let y1 = (time / 3) % height;
-  let y2 = y1 + 37;
-  let y3 = y1 - 13;
-
-  // Actually draw the triangle.
-  drawTriangle(x1, y1, x2, y2, x3, y3, 'purple', 5);
-};
 
 // Leave this code here or the animation won't run. Also don't change the name
 // of drawFrame either here or where it is defined. (Or, if you must, change it
