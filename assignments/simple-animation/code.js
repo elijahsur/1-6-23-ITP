ec9265d196d26bb1b362df2dd36ce1dccaf31bbc
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-08-29 14:55:44 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -1,5 +1,5 @@
 const drawFrame = (time) => {
-  drawFilledCircle((time / 5) % width, height, 50, 'blue');
+  drawFilledCircle((time / 5) % width + 10, height, 50, 'blue');
   drawFilledCircle((time / 5) % width, 0, 50, 'blue');
   drawFilledCircle((time / 5) % width, height, 50, 'red');
   drawFilledCircle((time / 5) % width, 0, 50, 'red');
