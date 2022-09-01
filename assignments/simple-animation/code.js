c7ad23f15b87756177a4d791dfb1e41bec7f24c1
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-01 14:51:17 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -1,5 +1,5 @@
 const drawFrame = (time) => {
   drawLine(width, height, 0, 0, 'blue', 5);
-  drawFilledSquare((time / 5) % width - 54, height, 500, 'white');
+  drawFilledRectangle((time / 5) % width - 54, height, 500, 'white');
 }
 animate(drawFrame);
