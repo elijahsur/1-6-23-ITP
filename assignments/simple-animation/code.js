782b56b0a12cd4a26452e0e6c3f4b330991ec9e0
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-02 15:08:11 -0700

    Updating assignments/simple-animation/code.js

--- assignments/simple-animation/code.js
+++ assignments/simple-animation/code.js
@@ -53,6 +53,7 @@ const drawFrame = (time) => {
     drawFilledCircle((time/10 + 1760)%width, (time/10 + 1760)%height, 25, 'white');
     drawFilledCircle((time/10 + 1800)%width, (time/10 + 1800)%height, 25, 'blue');
     drawFilledCircle((time/10 + 1840)%width, (time/10 + 1840)%height, 25, 'black');
+    drawFilledCircle((time/10 + 1880)%width, (time/10 + 1880)%height, 25, 'red');
 }
 
 // This is a function that we define to make it easier to draw a triangle. You
