9029269ca61d49c8f23554c3a5e94a5f6d9a1e16
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-08 14:54:24 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -4,3 +4,5 @@ const multiply = (a,b) => (a*b);
 const divide = (a,b) => (a/b);
 const mod = (a,b) => (a%b);
 const averageOfTwo = (a,b) => ((a+b)/2)
+const averageOfThree = (a,b,c) => ((a+b+c)/3)
+const distance = (a,b) => (a-b)
