7efd6ffe760430064d2fcbcd2b806fa8573537b6
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-20 14:45:36 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -24,12 +24,13 @@
  *  clear()
  */
 
-let radius = 25;
+let radius = 22;
 let dif = radius;
 let x = 0;
-while (x < Math.floor(width / (radius * 2))) {
+//while (x < Math.floor(width / (radius * 2))) {
 
   drawFilledCircle(dif, height / 2, radius, 'red')
   x = x + 1
   dif = dif + radius * 2
 }
+
