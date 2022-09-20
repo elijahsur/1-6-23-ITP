db49c3524132c81c02dd70c189c63606f27b263e
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-20 14:54:00 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,7 +28,7 @@
 
 const linecircle = (r) => {
   while (x < Math.floor(width / (r * 2))) {
-    var r = 5
+  let r = 5
   drawFilledCircle(dif, height / 2, r, 'red')
   x = x + 1
   dif = dif + radius * 2
