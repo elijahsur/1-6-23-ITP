7f0af346d9d846337c96143de758a101df61e69a
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-26 17:45:46 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -76,6 +76,6 @@ const bigCircle = (count) => {
       color = 'red';
     }
     alternate = alternate + 1
-    factor = factor - (width/(radius*2) / count * c)
+    factor = factor - (width/(height*2) / count * c)
   }
 }
