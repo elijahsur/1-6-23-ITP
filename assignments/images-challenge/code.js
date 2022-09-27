bbe02521ad0438f16af82a077c81a5079d224d3f
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-26 17:41:12 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -28,7 +28,7 @@
 
 const forlineOfCircles = (radius) => {
   let dif = radius;
-  while (let c = 0; c < Math.floor(width / (radius * 2)); c++) {
+  for (let c = 0; c < Math.floor(width / (radius * 2)); c++) {
     drawFilledCircle(dif, height / 2, radius, 'red')
     x = x + 1
     dif = dif + radius * 2
