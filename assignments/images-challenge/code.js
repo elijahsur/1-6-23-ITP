225112e33f6744f447c9e58aa241c2bdc248bcbc
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-09-29 15:05:30 -0700

    Updating assignments/images-challenge/code.js

--- assignments/images-challenge/code.js
+++ assignments/images-challenge/code.js
@@ -89,7 +89,7 @@ const checkB = (count) => {
 
 const lineC = (count) => {
   let xaxis = 0;
-  let yaaxis = 0;
+  let yaxis = 0;
   for (let c = 0; c < width/count + 1;c++) {
     drawLine(0, 0, xaxis, yaxis, 'black', 1)
     xaxis = xaxis + (width/count)
