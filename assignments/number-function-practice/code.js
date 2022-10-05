bc94709a46e354dca0e6de7f424f6dc4a7b53d1c
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-05 15:24:46 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,3 +1,18 @@
 const sum = (a,b) => {
   return a+b
 }
+const difference = (a,b) => {
+  return a-b
+}
+const product = (a,b) => {
+  return a*b
+}
+const ratio = (a,b) => {
+  return a/b
+}
+const remainder = (a,b) => {
+  return a%b
+}
+const average = (a,b) => {
+  return (a+b)/2
+}
