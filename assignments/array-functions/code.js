5597d3d21759ca4f28e5d6435103ecfc529c9e92
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-14 15:26:21 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,3 +1,9 @@
-const countTens = () => {
-  
+const countTens = (numbers) => {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 10) {
+      count++;
+    }
+  }
+  return count
 }
