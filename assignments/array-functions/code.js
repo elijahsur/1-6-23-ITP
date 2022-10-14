47ecf61088b458e5e098492e3aff07ecffb5e6d7
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-14 15:21:48 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,5 +1,3 @@
-const anyOverOneHundred = [n1,n2,n3,n4];
-anyOverOneHundred[n1] = 'adwas'
-anyOverOneHundred[n2] = '3tuaiw'
-anyOverOneHundred[n3] = 'wiafna'
-anyOverOneHundred[n4] = 'wifjawn'
+const countTens = () => {
+  
+}
