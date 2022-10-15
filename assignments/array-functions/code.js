8b12ddf50a66a0f73f4d5c8e4d1a5556f2e3f448
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-15 16:57:48 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -18,7 +18,7 @@ const sum = (numbers) => {
 
 const evens = (numbers) => {
     const evens = []
-  for (let i = 0; o < numbers.length; i++) {
+  for (let i = 0; i < numbers.length; i++) {
     if (numbers[i] % 2 == 0) {
       evens.push(numbers[i])
     }
