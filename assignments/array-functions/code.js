c8c98c69fe9b983d5b8bc4504bc191a92df5e9e5
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-15 16:51:22 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -23,7 +23,7 @@ const evens = () => {
 const anyOverOneHundred = (numbers) => {
   let count = 0;
   for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] === 100) {
+    if (numbers[i] >= 100) {
       count++;
     }
   }
