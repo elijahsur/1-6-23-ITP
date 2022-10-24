9e7040e4d7bd3c29d1742bc9e0127010ead31306
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-24 15:21:43 -0700

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -6,5 +6,5 @@
 // weeks but for now you can just adapt this code.
 
 registerOnclick((x, y) => {
-  drawText('bryan camacho', x, y, 'black', Math.min(width, height) * 0.3);
+  drawText('brootay', x, y, 'black', Math.min(width, height) * 0.3);
 });
