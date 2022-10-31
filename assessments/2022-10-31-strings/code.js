9ccc40f7e9b2cc15829ddee7228e7068ed287a7f
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-10-31 14:54:08 -0700

    Updating assessments/2022-10-31-strings/code.js

--- assessments/2022-10-31-strings/code.js
+++ assessments/2022-10-31-strings/code.js
@@ -11,5 +11,5 @@ const rand = (n) => Math.floor(Math.random() * n);
 // function but you can get partial credit for a function that is basically
 // correct even if contains small mistakes.
 const firstHalf = (string) => {
-  return string.substring(0,s.length/2)
+  return string.substring(0,string.length/2)
 }
