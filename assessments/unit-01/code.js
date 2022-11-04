071a083e6ee1f54e3c0d0cf1244d26999ff096af
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-04 15:24:25 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -42,12 +42,7 @@ const upDownLastCharacter = (whole) => {
 }
 
 const yesIfEven = (number) => {
-  if (number % 2 == 0) {
-    return 'yes'
-  };
-  if (number % 2 == 1) {
-    return 'no'
-  };
+return number % 2 === 0 ? 'yes' | 'no'
 }
 
 
