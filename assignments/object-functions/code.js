4be835af4bbd8455c0c8660b419d1532bf092630
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-07 15:01:48 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -9,3 +9,6 @@ const emptyObject = () => {
   let object = {}
   return object
 }
+const distance = (point1,point2) => {
+  return Math.sqrt((point1['x']-point2['x'])**2(+point1['y']-point2['y'])**2)
+}
