4522bbead4ca5f1572c3de2c43ae101659e37b26
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-08 15:17:18 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -33,6 +33,6 @@ const newHighScore = (currenths, array) => {
   return highscore
 }
 const summarizeBooks = (array) => {
-  let summary = array[1.title]
+  let summary = array[1]
   return summary
 }
