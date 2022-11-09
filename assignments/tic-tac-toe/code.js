01e4aabed17bfaf594c18f16729c8dd53579f1b8
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-09 14:54:25 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -23,4 +23,4 @@ registerOnclick((x, y) => {
 });
 
 drawLine(width / 1.5, height, width / 1.5, 0, 'black', 4)
-drawLine(width / 1.5 * 2, height, width / 1.5 * 2, 'black', 4)
+drawLine(width / 1.5 + width / 1.5, width / 1.5 * 2, height, 'black', 4)
