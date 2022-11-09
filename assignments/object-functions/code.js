aed32a40e210a94ee08b59a9b99024db126a48fe
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-09 14:49:25 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -2,19 +2,16 @@ const getX = (object) => {
   return object['x']
 }
 const point = (x, y) => {
-  let object = { "x": x, "y": y }
-  return object
+  return { "x": x, "y": y }
 }
 const emptyObject = () => {
-  let object = {}
-  return object
+  return {}
 }
 const distance = (pointa, pointb) => {
   return Math.sqrt((pointa['x'] - pointb['x']) ** 2 + (pointa['y'] - pointb['y']) ** 2)
 }
 const midpoint = (pointa, pointb) => {
-  let midpoint = { 'x': (pointa.x + pointb.x) / 2, 'y': (pointa.y + pointb.y) / 2 }
-  return midpoint
+  return { 'x': (pointa.x + pointb.x) / 2, 'y': (pointa.y + pointb.y) / 2 }
 }
 const sumSalaries = (array) => {
   let total = 0;
