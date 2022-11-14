a2a6289ffe3da010ab2a2786f913b185c7c3bc2a
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-14 14:59:11 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -14,6 +14,11 @@ const higherPaid = (e1, e2) => {
 };
 
 const isSamePoint = (p1, p2) => {
+  if (p1.x = p2.x, p1.y = p2.y) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const totalWithTip = (bill, tipPercentage) => {
