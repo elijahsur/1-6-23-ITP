786edf2d0e879fdc514f155df4143ef2faaa131c
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-15 15:07:07 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,16 +1,19 @@
 const findFnord = (string) => {
   return string.indexOf('fnord')
 }
-const stringContains = (string1,string2) => {
+const stringContains = (string1, string2) => {
   if (string1.indexOf(string2) != -1) {
     return true
-  }  else {
+  } else {
     return false
   }
 }
 const firstAndLast = (string) => {
-  return string[0]+string[string.length - 1]
+  return string[0] + string[string.length - 1]
 }
 const swapFrontAndBack = (string) => {
-  return string.substring(string.length / 2, string.length) + string.substring(0,string.length/2)
+  return string.substring(string.length / 2, string.length) + string.substring(0, string.length / 2)
+}
+const simplePigLatin = (string, number) => {
+  return string + 'ay'
 }
