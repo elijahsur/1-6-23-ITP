5dbc34df8e40e138e9322e5981edf5fe684bb556
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-15 14:53:01 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -2,54 +2,24 @@
 // should save you some time.
 
 const area = (rect) => {
-  return rect.width * rect.height
 };
 
 const higherPaid = (e1, e2) => {
-  if (e1.salary > e2.salary){
-    return e1
-  } else {
-    return e2
-  }
 };
 
 const isSamePoint = (p1, p2) => {
-  if (p1.x = p2.x) {
-    if (p1.y = p2.y) {
-      return true
-    }
-  } else {
-    return false
-  }
 };
 
 const totalWithTip = (bill, tipPercentage) => {
-  return {subtotal: bill.subtotal, tip: bill.subtotal*tipPercentage, total: bill.subtotal + bill.subtotal*tipPercentage}
 };
 
 const isWinner = (player) => {
-  if (player.score > 100) {
-    return true
-  } else {
-    return false
-  }
 };
 
 const updateWins = (players) => {
-  for (i = 0; i < players.length; i++) {
-    if (players[i].score > 100) {
-      players[i].wins = players[i].wins + 1
-    }
-  }
 };
 
 const bigWinners = (players) => {
-  let bigWins = []
-  for (i = 0; i < players.length; i++) {
-    if (players[i].wins > 10) {
-      bigWins.push(players[i])
-    }
-  }
 };
 
 const fillTimesTable = (table) => {
