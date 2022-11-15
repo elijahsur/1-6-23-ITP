57b843aea5dde97630e50798d2c171b1831b392a
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-15 15:02:41 -0800

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,3 +1,10 @@
 const findFnord = (string) => {
   return string.indexOf('fnord')
 }
+const stringContains = (string1,string2) => {
+  if (string1.indexOf(string2) != -1) {
+    return true
+  }  else {
+    return false
+  }
+}
