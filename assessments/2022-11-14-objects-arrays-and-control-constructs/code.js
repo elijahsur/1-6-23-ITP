c437b7a83075ae94ebfd2d68c33475e9f7ecc397
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-18 15:32:25 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -75,17 +75,17 @@ const sums = (n) => {
 
 const rule110 = (cells) => {
   let newarray = []
-  newarray[0] = 0
+  
   for (let i = 1; i < cells.length; i++) {
-    if (cells[i - 1] = 0) {
-      if (cells[i] = 0) {
-        if (cells[i + 1] = 0) {
+    if (cells[i - 1] === 0) {
+      if (cells[i] === 0) {
+        if (cells[i + 1] === 0) {
           newarray.push(0)
         }
       }
     } else {
-      if (cells[i] = 0) {
-        if (cells[i + 1] = 0) {
+      if (cells[i] === 0) {
+        if (cells[i + 1] === 0) {
           newarray.push(0)
         }
       } else {
