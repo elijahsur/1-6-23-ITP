c9adcea7a0a5047d42e1d87ce7df9671807814b4
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-11-30 14:51:46 -0800

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -10,29 +10,44 @@ const rand = (n) => Math.floor(Math.random() * n);
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
 
-const fireAlarm = (pulled,smoke,planned) => {
+const fireAlarm = (pulled, smoke, planned) => {
   return !pulled || !smoke || !planned
 }
 
-const canBePresident = (age,citizen,years) => {
+const canBePresident = (age, citizen, years) => {
   if (age >= 35) {
     if (citizen === true) {
       if (years >= 14) {
         return true
-      } else {return false}
-    } else {return false}
-  } else {return false}
+      } else { return false }
+    } else { return false }
+  } else { return false }
+}
+const willSeeTweet = (followed, retweet, blocked) => {
+  if (followed === true) {
+    if (blocked === false) {
+      return true
+    } else { return false }
+  } else {
+    if (retweet === true) {
+      if (blocked === false) {
+        return true
+      } else {
+        return false
+      }
+    }
+  }
 }
 const firstAndLast = (string) => {
-  return string[0]+string[string.length - 1]
+  return string[0] + string[string.length - 1]
 }
 
 const swapFrontAndBack = (string) => {
-  return string.substring(string.length/2, string.length) + string.substring(0,string.length/2)
+  return string.substring(string.length / 2, string.length) + string.substring(0, string.length / 2)
 }
 
-const simplePigLatin = (string,number) => {
-  return string.substring(number,string.length)+string.substring(0,number-1)+'ay'
+const simplePigLatin = (string, number) => {
+  return string.substring(number, string.length) + string.substring(0, number - 1) + 'ay'
 }
 
 const randomCharacter = (string) => {
