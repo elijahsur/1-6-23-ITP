f64298f2f1f0310d0cd67577d4c7d015b4de4566
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-02 14:36:23 -0800

    Updating assessments/2022-12-02-booleans/code.js

---
+++ assessments/2022-12-02-booleans/code.js
@@ -0,0 +1,7 @@
+const needHeavyCoat = (goingOutside, isCold) => {
+  if (goingOutside === true) {
+    if (isCold === true) {
+      return true
+    } else {return false}
+  } else {return false}
+}
