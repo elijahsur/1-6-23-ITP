6a010c5924fe9aabd2c3cfbad156ca34e671d1bd
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-02 14:54:23 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -2,8 +2,8 @@ const needHeavyCoat = (goingOutside, isCold) => {
   if (goingOutside === true) {
     if (isCold === true) {
       return true
-    } else {return false}
-  } else {return false}
+    } else { return false }
+  } else { return false }
 }
 const needSunscreen = (goingToBeach, goingSkiing) => {
   return goingToBeach || goingSkiing
@@ -12,24 +12,24 @@ const needMittens = (goingOutside, isWarm) => {
   if (goingOutside === true) {
     if (isWarm === false) {
       return true
-    } else {return false}
-  } else {return false}
+    } else { return false }
+  } else { return false }
 }
 const isVenemous = (striped, blueHead) => {
   if (blueHead === true) {
     if (striped === false) {
       return false
-    } else {return true}
-  } else {return true}
+    } else { return true }
+  } else { return true }
 }
 const okaySpeed = (speedLimit, speedOfCar) => {
-  return (speedOfCar - 10) < speedLimit || (speedOfCar + 10) > speedLimit
+  return speedOfCar < speedLimit - 10 || speedOfCar > speedLimit + 10
 }
 const twiceAsExpensive = (price1, price2) => {
-  return price1 > price2*2
+  return price1 > price2 * 2
 }
 const winningRecord = (numberOfWins, numberOfLosses, numberOfTies) => {
-  return numberOfWins > (numberOfWins + numberOfLosses + numberOfTies)/2
+  return numberOfWins > (numberOfWins + numberOfLosses + numberOfTies) / 2
 }
 const isMagicNumber = (number) => {
   return number === 42 || number === 17
