bb9efa8e156e4a051d40502abb2aefc8ae0f8ea6
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-02 14:41:27 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -8,3 +8,17 @@ const needHeavyCoat = (goingOutside, isCold) => {
 const needSunscreen = (goingToBeach, goingSkiing) => {
   return goingToBeach || goingSkiing
 }
+const needMittens = (goingOutside, isWarm) => {
+  if (goingOutside === true) {
+    if (isWarm === false) {
+      return true
+    } else {return false}
+  } else {return false}
+}
+const isVenemous = (striped, blueHead) => {
+  if (blueHead === true) {
+    if (striped === false) {
+      return false
+    } else {return true}
+  } else {return true}
+}
