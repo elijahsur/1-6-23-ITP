50c5e5e9cd25ea0aa753bfd14afa853e851b9bb6
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-07 15:13:21 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -122,7 +122,7 @@ const sumOfSquares = (arg) => {
 }
 const pairs = (arg) => {
   for (let i = 0; i - 1< arg; i++) {
-    let a = nextNumber(i)
+    let a = i + 1
     pair(i, a)
     pair(i, i)
     pair(a, i)
