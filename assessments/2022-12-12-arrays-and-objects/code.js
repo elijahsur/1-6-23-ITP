91f37edbe09c9972da2d78b48a4d8a1e09c3fe70
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-12 15:15:45 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -9,7 +9,7 @@ const makeBoard = () => {
   ]
 }
 const makeMove = (mark, row, column) => {
-  return (mark, row, column)
+  return ('mark' = mark, 'row' = row, 'column' = column)
 }
 const placeMark = (array, object) => {
   array[object.row[object.column]] = object.mark
