004c4308fc98fae106806f1a66c1fbb2780373c7
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-12 15:14:26 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -17,6 +17,4 @@ const placeMark = (array, object) => {
 const allTheSame = (array) => {
   return array[1] === array[2] === array[3]
 }
-let board = makeBoard();
-let move = makeMove('x', 1, 1);
-placeMark(board,move)
+const extractColumn = () => {}
