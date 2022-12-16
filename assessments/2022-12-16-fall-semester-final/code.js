ef614a7690858f955c99ccffe6ac2f24382adb8d
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-16 11:25:42 -0800

    Updating assessments/2022-12-16-fall-semester-final/code.js

--- assessments/2022-12-16-fall-semester-final/code.js
+++ assessments/2022-12-16-fall-semester-final/code.js
@@ -164,3 +164,6 @@ const needHeavyCoat = (goingOutside, somewhereCold) => {
 const needSunscreen = (goingToBeach, goingSkiing) => {
   return goingToBeach || goingSkiing
 }
+const needMittens = (goingOutside, somewhereWarm) => {
+  return goingOutside && !somewhereWarm
+}
