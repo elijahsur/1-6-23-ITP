7aa91a9fc296b8ab8f67cb438c771ce9d9d50ba7
elijahsur <111320647+elijahsur@users.noreply.github.com>
2022-12-16 10:55:41 -0800

    Updating assessments/2022-12-12-arrays-and-objects/code.js

--- assessments/2022-12-12-arrays-and-objects/code.js
+++ assessments/2022-12-12-arrays-and-objects/code.js
@@ -15,6 +15,3 @@ const makeMove = (xorO, row, column) => {
 const placeMark = (board, move) => {
   board[move.row][move.column] = move.mark
 }
-let board = makeBoard();
-  let move = makeMove('X', 1, 1);
-  placeMark(board, move);
