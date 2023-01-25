43ed526e45187eb37d5ac96c677edc4acc4e2e99
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-01-25 23:30:09 +0000

    finished color; now making timer for last hit 10

--- projects/web-demos/public/click-counter/script.js
+++ projects/web-demos/public/click-counter/script.js
@@ -1,24 +1,43 @@
 let counter = 0;
+let seconds = 0;
 const randomRGB = () =>
   Array(3)
     .fill()
     .map(() => Math.floor(Math.random() * 255));
-//i want to try to add a random color to the number every 10 numbers, but im not sure how to use this randomRGB function
+
 const rgb = (r, g, b) => `rgb(${r}, ${g}, ${b})`;
+
+const randomRGBColor = () => {
+  const values = randomRGB();
+  return rgb(values[0], values[1], values[2]);
+};
+const body = document.querySelector('body')
+const lastClicked = document.createElement('p')
+body.append(lastClicked)
 document.querySelector('p').onclick = (e) => {
   const p = e.target;
   counter++;
   if (counter === 1) {
-    p.innerHTML = 'I have been clicked <span id="counter"></span> time</span>.';
+    p.innerHTML = 'I have been clicked <span id="counter"></span> time.';
   }
   if (counter > 1) {
-    p.innerHTML = 'I have been clicked <span id="counter"></span> times</span>.'
+    p.innerHTML = 'I have been clicked <span id="counter"></span> times.'
   }
-  if (counter/10 === Math.floor(counter/10)) {
-    p.innerHTML = 'I have been clicked <span style="color:red"><span id="counter"></span></span> times</span>!'
+  if (counter % 10 === 0) {
+    p.innerHTML = `I have been clicked <span style="color:${randomRGBColor()}"><span id="counter"></span></span> times!`
+    if (counter === 10) {
+      lastClicked.textContent = 'I last reached 10 0 seconds ago!'
+      seconds = date.now() / 1000
+    } else {
+    let currentSeconds = Date.now() / 1000
+    let difference = currentSeconds - seconds
+    seconds = Date.now() / 1000
+    lastClicked.textContent = 'I last reached 10 ' + difference + ` seconds ago!`
+    // how would i make "difference" only include the decimal spots up to the first 2-ish? just to simplify the number
+    }
   }
-  if (counter/100 === Math.floor(counter/100)) {
-    p.innerHTML = 'I have been clicked <span style="color:yellow"><span id="counter"></span></span> times</span>!'
+  if (counter % 100 === 0) {
+    p.innerHTML = 'I have been clicked <span style="color:yellow"><span id="counter"></span></span> times!'
   }
   p.querySelector('#counter').textContent = `${counter}`;
 };
