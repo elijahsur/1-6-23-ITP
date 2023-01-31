c4fbb58782e8f5b7bb5cf865f8deb73af3b29c17
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-01-31 23:28:32 +0000

    code in while loop not done; started recoding

--- projects/genetic-algorithm/public/scripts/algorithm.js
+++ projects/genetic-algorithm/public/scripts/algorithm.js
@@ -8,23 +8,41 @@ const randomPhrase = (length) => {
         }
         return generation
 }
-let algorithm = (text) => {
-    // generates random list of characters the same length as the text
-    let answerarray = []
-    answerarray.push(randomPhrase(text.length))
-    let limit = 1000;
-    let previous = answerarray[answerarray.length - 1]
-    while (answerarray[answerarray.length - 1] != text && limit-- > 0) {
-        let generation = ''
-        for (let j = 0; j < previous.length; j++) {
-            if (answerarray[answerarray.length - 1][j] === text[j]) {
-                generation += answerarray[answerarray.length - 1][j]
-            } else {
-                generation += alphabet[Math.floor(Math.random() * 27)]
+const fitness = (generated, text) => {
+    let fitness = 0
+    for (let i = 0; i < text.length; i++) {
+        if (generated[i] === text[i]) {
+            fitness++
+        }
+    }
+    return fitness/text.length
+}
+let algorithm = (text, batch) => {
+    // temp contains the newest batch of 
+    let temp = []
+    let finalarray = []
+    let done = false
+    //provides the first batch
+    for (let i = 0; i < batch; i++) {
+        let phrase = randomPhrase(text.length)
+        temp.push([phrase, fitness(phrase, text)])
+    }
+    while (done === false) {
+        // will determine the top 25% of the batch
+        let temparray = []
+        for (let j = 0; j < temp.length; j++) {
+            temparray.push[temp[j][1]]
+            for (let k = 0; k < temparray.length; k++) {
+                if (temparray[j][1] > temparray[k][1]) {
+                    temparray.index(k) = temparray[j]
+                }
             }
         }
-        answerarray.push(generation)
+
+        done = true
+        console.log(temparray)
     }
-    return answerarray
+    console.log(temp)
+    console.log(temp[3][1])
 }
-console.log(algorithm('foobar'))
+console.log(algorithm('foobar',20))
