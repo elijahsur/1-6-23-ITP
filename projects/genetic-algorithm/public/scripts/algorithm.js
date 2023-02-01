5b8a3ad923d0172dd51d2e7c6870b208bcbb05f2
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-01 23:31:24 +0000

    reorganized code, working on top25 function

--- projects/genetic-algorithm/public/scripts/algorithm.js
+++ projects/genetic-algorithm/public/scripts/algorithm.js
@@ -1,48 +1,71 @@
 let alphabet = 'abcdefghijklmnopqrstuvwxyz '
 
+const test = (batch,string) => {
+    return top25(firstbatch(batch,string))
+}
+
+// constructs a random phrase, with "length" as the length of the string
 const randomPhrase = (length) => {
         // generates random list of characters the same length as the text
         let generation = ''
         for (let i = 0; i < length; i++) {
-            generation = generation + alphabet[Math.floor(Math.random() * 27)]
+            generation = generation + alphabet[Math.floor(Math.random() * alphabet.length)]
         }
         return generation
 }
+// creates the first batch of random strings
+const firstbatch = (batchamount,text) => {
+    let batcharray = []
+    for (let i = 0; i < batchamount; i++) {
+        let phrase = randomPhrase(text.length)
+        batcharray.push([phrase, fitness(phrase, text)])
+    }
+    return batcharray
+}
+// judges the fitness of a group of strings
 const fitness = (generated, text) => {
     let fitness = 0
     for (let i = 0; i < text.length; i++) {
-        if (generated[i] === text[i]) {
+        if (generated[i][0] === text[i]) {
             fitness++
         }
     }
     return fitness/text.length
 }
+// determines the top 25% of a batch, with "string" being the desired string. currently reorders the batch from highest to lowest
+const top25 = (batch) => {
+    let top25 = []
+    for (let i = 0; i < batch.length; i++) {
+        for (let j = 0; j < top25.length; j++) {
+            if (top25[j][1] < top25[i][1]) {
+                top25[j][1] = top25[i][1]
+                top25.push(batch[j][1])
+            } else {
+                top25.push(batch[i][1])
+            }
+        }
+    }
+    return top25
+}
+
 let algorithm = (text, batch) => {
-    // temp contains the newest batch of 
-    let temp = []
+    let batcharray = []
     let finalarray = []
     let done = false
     //provides the first batch
-    for (let i = 0; i < batch; i++) {
-        let phrase = randomPhrase(text.length)
-        temp.push([phrase, fitness(phrase, text)])
-    }
+    firstbatch(batch)
     while (done === false) {
         // will determine the top 25% of the batch
         let temparray = []
-        for (let j = 0; j < temp.length; j++) {
-            temparray.push[temp[j][1]]
+        for (let j = 0; j < batcharray.length; j++) {
+            temparray.push[batcharray[j][1]]
             for (let k = 0; k < temparray.length; k++) {
                 if (temparray[j][1] > temparray[k][1]) {
                     temparray.index(k) = temparray[j]
                 }
             }
         }
-
+// for testing
         done = true
-        console.log(temparray)
     }
-    console.log(temp)
-    console.log(temp[3][1])
 }
-console.log(algorithm('foobar',20))
