8392a37f0325f9487227af9c0f70b6f9b5c552e6
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-02 23:27:24 +0000

    reworked top25 code, not done

--- projects/genetic-algorithm/public/scripts/algorithm.js
+++ projects/genetic-algorithm/public/scripts/algorithm.js
@@ -1,20 +1,20 @@
 let alphabet = 'abcdefghijklmnopqrstuvwxyz '
 
-const test = (batch,string) => {
-    return top25(firstbatch(batch,string))
+const test = (batch, string) => {
+    return top25(firstbatch(batch, string))
 }
 
 // constructs a random phrase, with "length" as the length of the string
 const randomPhrase = (length) => {
-        // generates random list of characters the same length as the text
-        let generation = ''
-        for (let i = 0; i < length; i++) {
-            generation = generation + alphabet[Math.floor(Math.random() * alphabet.length)]
-        }
-        return generation
+    // generates random list of characters the same length as the text
+    let generation = ''
+    for (let i = 0; i < length; i++) {
+        generation = generation + alphabet[Math.floor(Math.random() * alphabet.length)]
+    }
+    return generation
 }
 // creates the first batch of random strings
-const firstbatch = (batchamount,text) => {
+const firstbatch = (batchamount, text) => {
     let batcharray = []
     for (let i = 0; i < batchamount; i++) {
         let phrase = randomPhrase(text.length)
@@ -23,29 +23,28 @@ const firstbatch = (batchamount,text) => {
     return batcharray
 }
 // judges the fitness of a group of strings
-const fitness = (generated, text) => {
+const fitness = (dna, text) => {
     let fitness = 0
     for (let i = 0; i < text.length; i++) {
-        if (generated[i][0] === text[i]) {
+        if (dna[i] === text[i]) {
             fitness++
         }
     }
-    return fitness/text.length
+    return fitness / text.length
 }
-// determines the top 25% of a batch, with "string" being the desired string. currently reorders the batch from highest to lowest
+// i think this code will work, im just not sure how to make it sort it from highest to lowest, not lowest to highest
+// currently, it deletes the highest numbers, but keeps the lowest
 const top25 = (batch) => {
-    let top25 = []
-    for (let i = 0; i < batch.length; i++) {
-        for (let j = 0; j < top25.length; j++) {
-            if (top25[j][1] < top25[i][1]) {
-                top25[j][1] = top25[i][1]
-                top25.push(batch[j][1])
-            } else {
-                top25.push(batch[i][1])
-            }
-        }
-    }
-    return top25
+let fitnessOnly = []
+for (let i = 0; i < batch.length; i++) {
+    fitnessOnly.push(batch[i][1])
+}
+console.log(fitnessOnly.length)
+for (let j = 0; j < Math.floor(fitnessOnly.length / .25) ; j++) {
+    fitnessOnly.sort()
+    fitnessOnly.pop(j)
+}
+console.log(fitnessOnly)
 }
 
 let algorithm = (text, batch) => {
@@ -53,19 +52,9 @@ let algorithm = (text, batch) => {
     let finalarray = []
     let done = false
     //provides the first batch
-    firstbatch(batch)
+    firstbatch(batch, text)
     while (done === false) {
-        // will determine the top 25% of the batch
-        let temparray = []
-        for (let j = 0; j < batcharray.length; j++) {
-            temparray.push[batcharray[j][1]]
-            for (let k = 0; k < temparray.length; k++) {
-                if (temparray[j][1] > temparray[k][1]) {
-                    temparray.index(k) = temparray[j]
-                }
-            }
-        }
-// for testing
-        done = true
+ 
     }
 }
+console.log(test(20, 'pizza'))
