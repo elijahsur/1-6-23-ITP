fdef0373d9bf43cd922282d420eadcddfc66db8e
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-21 14:50:10 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -99,7 +99,9 @@ registerOnclick((x, y) => {
   let c;
 
   // Check if there's a winner already.
-  winner = checkWinner(r,c,lines,board);
+  if (checkWinner(r,c,lines,board)) {
+    winner = checkWinner(r,c,lines,board);
+  }
 
   r = Math.floor((y - boardTop) / cellSize);
   c = Math.floor((x - boardLeft) / cellSize);
@@ -117,8 +119,10 @@ registerOnclick((x, y) => {
     move++;
 
     // Check if there's a winner now
-    winner = checkWinner(r,c,lines,board);
-
+    winner = null;
+    if (checkWinner(r,c,lines,board)) {
+      winner = checkWinner(r,c,lines,board);
+    }
     if (winner !== null) {
       // Draw the line through three in a row
       const [r1, c1] = winner[0];
