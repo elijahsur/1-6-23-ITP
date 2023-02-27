abd143cb1f4246523fa790c981cf3ea02bd5e842
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-27 15:26:00 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -17,6 +17,11 @@ const map = (fn, array) => {
 };
 
 const flatMap = (fn, array) => {
+  let nArray = []
+  for (let i = 0; i < array.length; i++) {
+    nArray.push(fn(i))
+  }
+  return nArray
 };
 
 const reduce = (fn, initialValue, array) => {
