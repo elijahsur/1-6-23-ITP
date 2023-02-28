c6400d880ec956e5f2c20ef6997760fb54025b2d
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-28 14:38:19 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -34,7 +34,7 @@ const reduce = (fn, initialValue, array) => {
 };
 
 const every = (predicate, array) => {
-  for (let i = 1; i < array.length + 1; i++) {
+  for (let i = 0; i < array.length; i++) {
     if (predicate(array[i])) {
       return false
     }
