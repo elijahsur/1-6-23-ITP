b21e401a0bfdb2e75dd7961b158134018d7f0b58
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-28 15:16:24 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -1,5 +1,5 @@
 const evens = (a) => a.filter((a) => a % 2 === 0)
-const odds = (a) => a.filter((a) => a % 2 === 1)
+const odds = (a) => a.filter((a) => a % 2 == 1||-1)
 const big = (a) => a.filter((a) => a > 100)
 const names = (a) => a.flatMap((a) => a["name"])
 const grades = (a) => a.flatMap((a) => a["grade"])
@@ -10,7 +10,7 @@ const allEven = (a) => a.every((a) => a % 2 === 0)
 const someEven = (a) => a.some((a) => a % 2 === 0)
 const lengthOfNames = (a) => {
   let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  a.filter((a) => a[1])
+  a.filter((a) => a[0])
   a.map((a) => a.length)
   return a
 }
