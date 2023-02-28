25523f6dd180ee0bdd69a04e34f9462a34a48a08
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-28 15:14:55 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -10,7 +10,7 @@ const allEven = (a) => a.every((a) => a % 2 === 0)
 const someEven = (a) => a.some((a) => a % 2 === 0)
 const lengthOfNames = (a) => {
   let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  a.filter((a) => a[0])
+  a.filter((a) => a[1])
   a.map((a) => a.length)
   return a
 }
