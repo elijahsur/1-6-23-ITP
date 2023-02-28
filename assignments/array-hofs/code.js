efaa847664a23cebf5cfd0792a286ea44c1f856f
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-28 14:56:46 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -2,7 +2,7 @@ const evens = (a) => a.filter((a) => a % 2 === 0)
 const odds = (a) => a.filter((a) => a % 2 === 1)
 const big = (a) => a.filter((a) => a > 100)
 const names = (a) => a.flatMap((a) => a["name"])
-const grades = (a) => a.flatMap((a) => a["name"])
+const grades = (a) => a.flatMap((a) => a["grade"])
 const pairs = (a) => a.map((a) => [a, a])
 const averageGrade = (a) => a.flatMap((a) => a["grade"]).reduce((tot, a) => tot + a) / a.length
 const flatpairs = (a) => 
