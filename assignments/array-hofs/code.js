72b83718f59550159bd0c5a7bc9c2bddfdde3b0f
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-02-28 15:26:04 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -9,6 +9,7 @@ const flatpairs = (a) => a.flatMap((a) => [a, a])
 const allEven = (a) => a.every((a) => a % 2 === 0)
 const someEven = (a) => a.some((a) => a % 2 === 0)
 const lengthOfNames = (a) => {
+  let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   a.map((a) => a[0])
   return a
 }
