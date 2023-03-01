3fc315f0483f0fb50812737fe3d5696731ab68ee
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-03-01 14:42:28 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -11,7 +11,6 @@ const someEven = (a) => a.some((a) => a % 2 === 0)
 const lengthOfNames = (a) => {
   let uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   a.map((a) => a[0])
-  console.log(a)
   return a
 }
 lengthOfNames(['Fred','Barney','car','Wilma','rock','Betty','dinosaur'])
