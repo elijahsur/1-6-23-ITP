25809a05ac8dbe989a2be403727f139f35d2b7f5
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-10 15:21:12 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -13,3 +13,11 @@ const triangular = (n) => {
     return n + triangular(n - 1)
   }
 }
+
+const fibonacci = (n) => {
+  if (n === 0) {
+    return 1
+  } else {
+    return n + fibonacci(n - 1)
+  }
+}
