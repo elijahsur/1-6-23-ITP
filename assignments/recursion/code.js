1096d444a681503127e014335d4485d51bcbb1f1
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-11 15:32:49 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -66,6 +66,6 @@ const treeMap = (t, f) => {
   if (isLeaf(t)) {
     return f(t)
   } else {
-    return { 'left': f(t.left), 'right': f(t.right) }
+    return { 'left': treeMap(f(t.left)), 'right': treeMap(f(t.right)) }
   }
 }
