4b3f289fac52d3b36bbbf9e26ea202200d68706d
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-18 14:37:45 -0700

    Updating assignments/recursion-2/code.js

--- assignments/recursion-2/code.js
+++ assignments/recursion-2/code.js
@@ -69,7 +69,7 @@ const maximum = (ary) => {
     if (ary[0] < ary[1]) {
       return maximum(ary.slice(1))
     } else {
-      return maximum(ary[0] + ary.slice(1))
+      return maximum([ary[0]].concat(ary.slice(1)))
     }
   }
 }
