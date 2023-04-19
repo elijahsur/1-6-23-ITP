f85d426b2b8c059d4eb94f0a9231fda51c883e29
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-19 15:17:35 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -55,13 +55,13 @@ const sumNested = (aryN) => {
 }
 
 const searchNested = (ary, n) => {
-  if (ary === n) {
-    return true
-  } else if (ary.length === 0) {
-    return false
-  } else if (ary[0] === n) {
-    return true
-  } else {
-    return searchNested(ary.slice(1))
-  }
+ if (ary.length === 0) {
+   return false
+ } else if (ary[0] === n) {
+   return true
+ } else if (isNumber(ary[0])) {
+   return false
+ } else {
+   return searchNested(ary.slice(1))
+ }
 }
