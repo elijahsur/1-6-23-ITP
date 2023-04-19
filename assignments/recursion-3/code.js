ea00edf2a7cac5a517d78bb23d206ed662a2a442
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-19 14:39:49 -0700

    Updating assignments/recursion-3/code.js

--- assignments/recursion-3/code.js
+++ assignments/recursion-3/code.js
@@ -15,5 +15,11 @@ const sumSquares = (n) => {
 }
 
 const lucas = (n) => {
-  
+  if (n === 0) {
+    return 2
+  } else if (n === 1) {
+    return 1
+  } else {
+    return lucas(n - 2) + lucas(n - 1)
+  }
 }
