0c93246ab8679606caa4e6cc9f4bc52319d9ae76
elijahsur <111320647+elijahsur@users.noreply.github.com>
2023-04-25 15:11:08 -0700

    Updating assessments/2023-04-21-recursion/code.js

--- assessments/2023-04-21-recursion/code.js
+++ assessments/2023-04-21-recursion/code.js
@@ -1,40 +1,9 @@
 ////////////////////////////////////////////////////////////////
 // Write your code here ...
 
-
-
-
-////////////////////////////////////////////////////////////////
-// Functions you will use in your code. No need to touch these
-// or understand these beyond understanding what they do which
-// is described in the appropriate questions.
-
-const isLeaf = (o) => typeof o !== 'object' || (!(('left' in o) && ('right' in o)));
-
-const isPrime = (n) => {
-  const loop = (f) => f ** 2 > n || (n % f !== 0 && loop(f + 1));
-  return n > 1 && loop(2);
-}
-
-const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-
-const mod = (n, m) => ((n % m) + m) % m;
-
-const rotate = (char, places) => {
-  const lower = char.toLowerCase();
-  const i = alphabet.indexOf(lower);
-  if (i === -1) {
-    return char;
-  } else {
-    const rotated = alphabet[mod(i + places, alphabet.length)];
-    return lower === char ? rotated : rotated.toUpperCase();
-  }
-}
-
 // x
 const factorial = () => { }
 
-// x
 const fibonacci = (n) => {
   if (n === 0) {
     return 0
@@ -112,5 +81,32 @@ const map = (list, fnct) => {
     return { 'first': fnct(list.first), 'rest': null }
   } else {
     return { 'first': fnct(list.first), 'rest': map(list.rest, fnct) }
+  }
+}
+
+////////////////////////////////////////////////////////////////
+// Functions you will use in your code. No need to touch these
+// or understand these beyond understanding what they do which
+// is described in the appropriate questions.
+
+const isLeaf = (o) => typeof o !== 'object' || (!(('left' in o) && ('right' in o)));
+
+const isPrime = (n) => {
+  const loop = (f) => f ** 2 > n || (n % f !== 0 && loop(f + 1));
+  return n > 1 && loop(2);
+}
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+const mod = (n, m) => ((n % m) + m) % m;
+
+const rotate = (char, places) => {
+  const lower = char.toLowerCase();
+  const i = alphabet.indexOf(lower);
+  if (i === -1) {
+    return char;
+  } else {
+    const rotated = alphabet[mod(i + places, alphabet.length)];
+    return lower === char ? rotated : rotated.toUpperCase();
   }
 }
